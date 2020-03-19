d439afe7f91d1756c9b431b221ae64791d694eb6
Harish Rana <r.shivi@yahoo.com>
2020-03-19 15:19:00 +0530

    feat: add travis yml

--- server/lib/index.js
+++ server/lib/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import initDb from './db/mongoose';
-import { DEV_NODE_SERVER_PORT } from './config';
+import { PORT } from './config';
 import problemRouter from './routers/problemRouter';
 
 const app = express();
@@ -8,8 +8,8 @@ app.use(express.json());
 app.use(problemRouter);
 
 initDb().then(() => {
-  app.listen(process.env.PORT || DEV_NODE_SERVER_PORT, () => {
-    console.log(`App listening on port ${DEV_NODE_SERVER_PORT}!`);
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
   });
 }).catch(error => {
   console.log(error);
