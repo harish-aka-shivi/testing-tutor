ef187dcd45cc4589cab38c9491b96dc18f73c444
Harish Rana <r.shivi@yahoo.com>
2020-03-26 00:07:09 +0530

    feat: change data source from db to file
    Also remove the db initialization

--- server/lib/controllers/problemController.js
+++ server/lib/controllers/problemController.js
@@ -1,44 +1,14 @@
-import Problem from '../models/problem';
-
-const addProblem = async (req, res) => {
-  const problem = new Problem({
-    ...req.body,
-  });
-  try {
-    await problem.save();
-    res.status(200).send(problem);
-  } catch (e) {
-    console.log(e);
-    res.status(500).send(`Error in saving problem ${e}`);
-  }
-};
+import problems from '../data/problems.json';
 
 const getAllProblems = async (req, res) => {
   try {
-    const problems = await Problem.find({});
-    res.status(200).send({ problems });
+    res.status(200).send(problems);
   } catch (error) {
     console.log(error);
     res.status(500).send(error);
   }
 };
 
-
-const getAProblem = async (req, res) => {
-  const problemId = req.params.id;
-  try {
-    const problem = await Problem.findById(problemId);
-    res.status(200).send({
-      problem,
-    });
-  } catch (error) {
-    console.log(error);
-    res.send(500).send(error);
-  }
-};
-
 export {
-  addProblem,
   getAllProblems,
-  getAProblem,
 };
