fe00811cba421d5cb85bbc887ff7d602a5fca926
Harish Rana <r.shivi@yahoo.com>
2020-03-26 00:07:09 +0530

    feat: change data source from db to file
    Also remove the db initialization

--- server/lib/index.js
+++ server/lib/index.js
@@ -1,13 +1,7 @@
-import initDb from './db/mongoose';
 import { PORT } from './config';
 import app from './app';
 
-initDb().then(() => {
-  app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
-  });
-}).catch(error => {
-  console.log(error);
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
 });
-
 export default app;
--- server/lib/routers/problemRouter.js
+++ server/lib/routers/problemRouter.js
@@ -1,16 +1,10 @@
 import express from 'express';
 import {
-  addProblem,
   getAllProblems,
-  getAProblem,
 } from '../controllers/problemController';
 
 const router = new express.Router();
 
 router.get('/problems', getAllProblems);
-
-router.get('/problem/:id', getAProblem);
-
-router.post('/problem', addProblem);
 
 export default router;
