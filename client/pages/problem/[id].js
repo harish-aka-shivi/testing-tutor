e7010ca8b04a3ef950ac992f46c016eaa6062d3a
Harish Rana <r.shivi@yahoo.com>
2020-03-30 16:35:27 +0530

    chore: add babel eslint for dynamic imports

--- client/pages/problem/[id].js
+++ client/pages/problem/[id].js
@@ -1,12 +1,11 @@
-/* eslint-disable no-underscore-dangle */
 import React from 'react';
 import { useRouter } from 'next/router';
 import dynamic from 'next/dynamic';
 
 const DynamicProblemDetail = dynamic(
   () => import('../../containers/ProblemDetail'),
-  { ssr: false }
-)
+  { ssr: false },
+);
 
 const Problem = () => {
   const router = useRouter();
